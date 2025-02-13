259ad20c188e079df87df86b26ac65ee57c853a4
dreammehedi <dreammehedihassan@gmail.com>
2025-02-13 08:59:19 +0600

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  // app.useGlobalInterceptors(new LoggingInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Crypto API')
+    .setDescription('Crypto API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  const port = 3000;
+  await app.listen(port, () => {
+    console.log(`API started on port ${port}`);
+  });
 }
 bootstrap();
